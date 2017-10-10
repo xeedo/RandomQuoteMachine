915ea3bb803929381d415d25a7b9c3b7b679d26b
Przemysław Grądzik <pggradzik@gmail.com>
2017-10-10 23:59:21 +0200

    Added Twitter button, styled getQuote button

--- assets/script.js
+++ assets/script.js
@@ -9,4 +9,25 @@ function getQuote() {
 function myFunc(myObj) {
   $("#quote").html(myObj[0].content);
   $("#author").html(myObj[0].title);
+
+  // remove existing button
+  $("#tweetBtn iframe").remove();
+
+  // generate new button
+  twttr.widgets.createShareButton(
+    "/",
+    document.getElementById("tweetBtn"),
+    {
+      text: getString(myObj).trim() + "\n--" + myObj[0].title,
+      size: "large"
+    }
+  );
+}
+
+function getString(obj) {
+  let txt = obj[0].content;
+  txt = txt.replace("<p>", "");
+  txt = txt.replace("</p>", "");
+
+  return txt;
 }
